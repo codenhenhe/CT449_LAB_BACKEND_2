a40f620fedcc38dbd5de9f82bd1d99af99dc7008
codenhenhe <linhnguyennhut07103@gmail.com>
2025-06-29 15:00:52 +0700

    setting handle create

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,10 +1,27 @@
-exports.create = (req, res) => {
-  res.send({ mesage: "Create handler" });
+const ApiError = require("../api-error");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) {
+    return next(new ApiError(400, "Name can not be empty"));
+  }
+
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
+    );
+  }
 };
 
 exports.findAll = (req, res) => {
   res.send({ mesage: "findAll handler" });
 };
+
 exports.findOne = (req, res) => {
   res.send({ mesage: "findOne handler" });
 };
